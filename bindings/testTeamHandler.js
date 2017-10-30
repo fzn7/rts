8a4f1ead0ae7f2107beef0a8ab37a24e36fd593b
Ilya Plotnikov <plotnikov@teamidea.ru>
2017-10-30 12:00:55 +0300

    [ISSUE-2] add game config, improve tests

--- bindings/testTeamHandler.js
+++ bindings/testTeamHandler.js
@@ -1,17 +1,20 @@
-var script = require('./Sim');
+var script = require('./PSim');
 var fs = require('fs');
 gameConfig = fs.readFileSync('gameConfig.tdf', 'utf8');
-
+console.log("GAME");
 var teamhandler = new script.CTeamHandler();
-var TdfParser = new script.TdfParser();
 var setup = new script.CGameSetup();
+console.log("Initialization");
 var init = setup.Init(gameConfig);
 console.log("Initialization success: " + init);
+ 
 var Obj = setup;
-
-teamhandler.SetAlly(1, 4, true);
-console.log(teamhandler.Ally(3, 2));
-
+ 
+ 
 teamhandler.LoadFromSetup(Obj);
 var c = teamhandler.ValidAllyTeam(0);
-console.log(c);
+console.log("ValidAllyTeam: " + c);
+ 
+ 
+teamhandler.SetAlly(1, 4, true);
+console.log("Ally: " + teamhandler.Ally(3, 2));
