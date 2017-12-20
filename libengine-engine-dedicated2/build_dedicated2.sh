8cf3c388d72ffb74530a65cb3625460c46b9427d
Ilya Plotnikov <plotnikov@teamidea.ru>
2017-12-20 16:16:39 +0300

    [CLEAN] Remove *.o files from rep

---
+++ libengine-engine-dedicated2/build_dedicated2.sh
@@ -0,0 +1,16 @@
+working_dir=`pwd`
+bindings_root=$working_dir/bindings/python-bindings-generator/
+
+echo "Current working_dir: $working_dir"
+echo "Bindings generator dir: $bindings_root"
+
+#cd $bindings_root/src
+#../build.sh
+#cp -f ../target/engineSim.idl $working_dir
+#cd $working_dir
+
+python /home/pi/emsdk-portable/emscripten/1.37.1/tools/webidl_binder.py engineSim.idl glue
+emcc $working_dir/libspring-dedicated2.a ./my_glue_wrapper.cpp -o $working_dir/index.js -I /home/pi/rts/spring-104.0.1/rts --post-js ./glue.js -std=c++14 -msse
+#emcc ./libengineSim/libengineSim. -o ./libengineSim/engineSim.js -I /home/pi/myrts/spring-104.0.1/rts --post-js ./glue.js -std=c++14 -msse
+rm glue.js
+rm glue.cpp
