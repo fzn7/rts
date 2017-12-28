66cb53bcbc7d18f7de3190116f144096180cd71d
Ilya Plotnikov <ilyaplotnikoff@gmail.com>
2017-12-28 21:09:24 +0300

    add files

---
+++ spring-104.0.1/tools/pr-downloader/src/lib/sha1/CMakeFiles/pr-sha1.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/rts/spring_103.0/tools/pr-downloader/src/lib/sha1/sha1.cpp" "/home/pi/rts/spring_103.0/tools/pr-downloader/src/lib/sha1/CMakeFiles/pr-sha1.dir/sha1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARCHIVE_SUPPORT"
+  "BOOST_NO_FENV_H"
+  "SPRING_DATADIR=\"/usr/local/share/games/spring\""
+  "STREFLOP_SSE"
+  "SYNCCHECK"
+  "THREADPOOL"
+  "USE_LIBSQUISH"
+  "_GLIBCXX_USE_NANOSLEEP"
+  "_RANDOM_TCC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
